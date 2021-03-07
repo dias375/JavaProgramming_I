3633e6eddac1b29e657b658da31b05eb73f0b220
dias375 <engrenangdias@gmail.com>
2021-03-07 20:13:22 -0300

    Part 6

--- part06-Part06_02.Stack/src/main/java/Stack.java
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -1,15 +1,37 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author rgd
  */
 public class Stack {
 
-    private Arraylist stack = new Arraylist();
+    private ArrayList<String> stack = new ArrayList<>();
 
+    public Stack() {
+    }
+
+    public void add(String value) {
+        this.stack.add(0, value);
+    }
+
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+
+    public ArrayList<String> values() {
+        return this.stack;
+    }
+
+    public String take() {
+        String topValue = this.stack.get(0);
+        this.stack.remove(0);
+        return topValue;
+    }
 }
--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,31 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    @Override
+    public String toString() {
+        if (this.elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        }
+
+        String firstSentence = "The collection " + this.name + " has ";
+
+        String secondSentence;
+        if (this.elements.size() == 1) {
+            secondSentence = this.elements.size() + " element:";
+        } else {
+            secondSentence = this.elements.size() + " elements:";
+        }
+
+        String thirdSentence = "\n";
+        for (String element : this.elements) {
+            thirdSentence += element;
+            if (this.elements.size() > 1) {
+                thirdSentence += "\n";
+            }
+        }
+
+        return firstSentence + secondSentence + thirdSentence;
+    }
+
 }
