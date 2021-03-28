364b401267091f8348b433f492d03d70995d0c1c
dias375 <engrenangdias@gmail.com>
2021-03-28 04:46:08 -0300

    Part 7

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,9 +1,61 @@
 
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
         // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+        MainProgram.sort(numbers);
+    }
+
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        for (int i : array) {
+            if (i < smallest) {
+                smallest = i;
+            }
+        }
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int index = 0;
+        int smallest = smallest(array);
+        for (int i = 0; i < array.length; i++) {
+            int currentNumber = array[i];
+            if (smallest == currentNumber) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        int smallest = table[startIndex];
+        int index = startIndex;
+        for (int i = startIndex; i < table.length; i++) {
+            if (table[i] < smallest) {
+                smallest = table[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int holder = array[index1];
+        array[index1] = array[index2];
+        array[index2] = holder;
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int index = indexOfSmallestFrom(array, i);
+            swap(array, i, index);
+            System.out.println(Arrays.toString(array));
+        }
+
     }
 
 }
--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,33 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        String[] arrayS = {"ac", "ay", "aa"};
+        System.out.println(Arrays.toString(arrayS));
+        sort(arrayS);
+        System.out.println(Arrays.toString(arrayS));
+    }
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
     }
 
 }
